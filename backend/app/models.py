459e5dbc1ee0102316f3919e87bcaf1c547a6692
Notascooby25 <andy.lettis@gmail.com>
2026-04-16 15:28:46 +0100

    note to notes

--- backend/app/models.py
+++ backend/app/models.py
@@ -44,7 +44,7 @@ class Mood(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     mood_score = Column(Integer, nullable=False)
-    note = Column(Text, nullable=True)
+    notes = Column(Text, nullable=True)
     timestamp = Column(DateTime(timezone=True), nullable=False)
     created_at = Column(DateTime(timezone=True), server_default=func.now(), nullable=False)
 
